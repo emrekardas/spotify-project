7694aba82aaf610d55db21faf943ceaa7288cef0
Emre KARDAŞ <emre.kardas@std.yeditepe.edu.tr>
2022-04-19 18:20:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from uuid import RESERVED_FUTURE
 from flask import Flask, request, url_for, session, redirect
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+import time
 
 app = Flask(__name__)
 
@@ -16,23 +17,62 @@ def login():
     auth_url = sp_oauth.get_authorize_url()
     return redirect(auth_url)
 
-@app.route('/redirect')
-def redirectPage():
+@app.route('/authorize')
+def authorize():
     sp_oauth = create_spotify_oauth()
     session.clear()
     code = request.args.get('code')
     token_info = sp_oauth.get_access_token(code)
-    session[TOKEN_INFO] = token_info
-    return redirect(url_for('getTracks', _external = True))
+    session["token_info"] = token_info
+    return redirect("/getTracks")
+
+@app.route('/logout')
+def logout():
+    for key in list(session.keys()):
+        session.pop(key)
+    return redirect('/')
 
 @app.route('/getTracks')
-def getTracks():
-    return "Some drake songs or something"
+def get_all_tracks():
+    session['token_info'], authorized = get_token()
+    session.modified = True
+    if not authorized:
+        return redirect('/')
+    sp = spotipy.Spotify(auth=session.get('token_info').get('access_token'))
+    results = []
+    iter = 0
+    while True:
+        offset = iter * 50
+        iter += 1
+        curGroup = sp.current_user_saved_tracks(limit=50, offset=offset)['items']
+        for idx, item in enumerate(curGroup):
+            track = item['track']
+            val = track['name'] + " - " + track['artists'][0]['name']
+            results += [val]
+        if (len(curGroup) < 50):
+            break
+
+def get_token():
+    token_valid = False
+    token_info = session.get("token_info", {})
+    
+    if not (session.get('token_info', False)):
+            token_valid = False
+            return token_info, token_valid
+    now = int(time.time())
+    is_token_expired = session.get('token_info').get('expires_at') - now < 60
+
+    if (is_token_expired):
+        sp_oauth = create_spotify_oauth()
+        token_info = sp_oauth.refresh_access_token(session.get('token_info').get('refresh_token'))
+
+    token_valid = True
+    return token_info, token_valid
 
 def create_spotify_oauth():
     return SpotifyOAuth(
         client_id = "517cdd50fdde4800a8c0dfbd5d3e216a",
         client_secret = "e9d577ebc7d34a7f852211ec3dd31bec",
-        redirect_uri = url_for('redirectPage', _external = True),
+        redirect_uri = url_for('authorize', _external = True),
         scope = "user-library-read"
     )
