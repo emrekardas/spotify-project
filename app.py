ea816ef8ec0d9c3b1af716e7cfc97da455213c21
Emre KARDAŞ <emre.kardas@std.yeditepe.edu.tr>
2022-04-19 09:30:41 +0300

    spotify oauth add

--- app.py
+++ app.py
@@ -1,30 +1,32 @@
-from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from shutil import register_unpack_format
+from uuid import RESERVED_FUTURE
+from flask import Flask, request, url_for, session, redirect
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 app = Flask(__name__)
 
+app.secret_key = "cOqrUR7Wtd"
+app.config['SESSION_COKKIE_NAME'] = 'Emre Cookie'
 
 @app.route('/')
-def index():
-   print('Request for index page received')
-   return render_template('index.html')
+def login():
+    sp_oauth = create_spotify_oauth()
+    auth_url = sp_oauth.get_authorize_url()
+    return redirect(auth_url)
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+@app.route('/redirect')
+def redirect():
+    return 'redirect'
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
+@app.route('/getTracks')
+def getTracks():
+    return "Some drake songs or something"
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
-
-
-if __name__ == '__main__':
-   app.run()
+def create_spotify_oauth():
+    return SpotifyOAuth(
+        client_id = "517cdd50fdde4800a8c0dfbd5d3e216a"
+        client_secret = "e9d577ebc7d34a7f852211ec3dd31bec"
+        redirect_uri= = url_for('https://emrekardas.azurewebsites.net//redirect', _external = True)
+        scope = "user-library-read"
+    )
